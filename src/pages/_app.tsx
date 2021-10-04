b8bae2423a279ab0e9c1a1dc4ead0e3e68ecec25
Jumpei Asanuma <jun64pei89@gmail.com>
2021-10-04 21:55:07 +0900

    add: Recoilの追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { AppProps } from 'next/app'
+import { RecoilRoot, atom, selector, useRecoilState, useRecoilValue } from 'recoil'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
     </ChakraProvider>
   )
 }
