ffeb3c9a4001b20afb8c4d82489b690f50200224
Jumpei Asanuma <jun64pei89@gmail.com>
2021-10-19 23:39:58 +0900

    update: recoilPersist追加

--- src/atoms/atom.ts
+++ src/atoms/atom.ts
@@ -1,4 +1,7 @@
 import { atom } from 'recoil'
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
 
 interface Todo {
   id: string
@@ -8,7 +11,9 @@ interface Todo {
 export const todosState = atom<Todo[]>({ 
   key: 'todosState',
   default: [],
-})
+  effects_UNSTABLE: [persistAtom]
+},
+)
 
 export const todosFilter = atom<string>({ 
   key: 'todosFilter',
