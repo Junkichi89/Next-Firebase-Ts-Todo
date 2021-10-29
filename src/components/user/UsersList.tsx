988225583e8b475ab8889be63ec844eac9c10cdc
Jumpei Asanuma <jun64pei89@gmail.com>
2021-10-30 00:37:18 +0900

    update: user一覧表示完成

---
+++ src/components/user/UsersList.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Link from 'next/link'
+import {
+  Flex,
+  ListItem,
+  OrderedList,
+  Text,
+} from '@chakra-ui/react'
+import UsersListItem from './UsersListItem'
+
+const UsersList: React.FC<any> = ({ users }: any) => {
+
+  return (
+    <OrderedList>
+      {users.map(user => (
+        <UsersListItem user={user} key={user.id} />
+      ))}
+
+    </OrderedList>
+  )
+}
+
+export default UsersList
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,1 +1,67 @@
-// ユーザー一覧を表示させる　ユーザー名と持っているTodoの数くらい表示しとく？
+// ユーザー一覧を表示させる　
+import {
+  Button,
+  Container,
+  Flex,
+  Heading,
+} from '@chakra-ui/react'
+import React, { useEffect, useState, } from 'react'
+import { collection, query, onSnapshot } from 'firebase/firestore'
+import { logout, useUser } from 'src/lib/auth'
+import Link from 'next/link'
+import UsersList from 'src/components/user/UsersList'
+import { db } from 'src/lib/firebase'
+
+interface Todo {
+  id: string
+  title: string
+  status: string
+}
+
+const UserList: React.FC = () => {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+
+    const q = query(collection(db, 'users'))
+
+    const unsub = onSnapshot(q, (snapshot) => {
+      const usersList: any = snapshot.docs.map((doc) => ({
+        id: doc.id, username: doc.data().displayName,
+      }))
+      setUsers(usersList)
+    })
+    return () => unsub()
+
+  }, [])
+
+
+  // ログインサンプル追加
+  const user = useUser()
+  console.log(user?.uid)
+
+  const handleLogout = (): void => {
+    logout().catch((error) => console.error(error))
+  }
+
+
+  return (
+    <>
+      <Container mt="200px" border="1px solid" borderRadius="5px" p="20px">
+        <Flex justify="space-between" align="center" pb="20px">
+          <Heading>Next Todo</Heading>
+          <Link href="/create" passHref>
+            <Button bg="lightblue">
+              <a>
+                作成
+              </a>
+            </Button>
+          </Link>
+        </Flex>
+        <UsersList users={users} />
+      </Container>
+    </>
+  )
+}
+
+export default UserList
