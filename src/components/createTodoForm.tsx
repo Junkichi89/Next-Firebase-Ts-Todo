2eff34f1f4c77e175d8d310d0fa08d80b3723ff6
Jumpei Asanuma <jun64pei89@gmail.com>
2021-10-29 21:55:12 +0900

    add: 新しいTodoの作成フォーム完成

---
+++ src/components/createTodoForm.tsx
@@ -0,0 +1,58 @@
+import {
+  InputGroup,
+  Input,
+  Button,
+  VStack,
+  Textarea,
+} from '@chakra-ui/react'
+import { useForm } from 'react-hook-form'
+import React, { ChangeEvent, useState } from 'react'
+import { db } from '../lib/firebase'
+import { addDoc, collection } from 'firebase/firestore'
+import { useRouter } from "next/router"
+
+interface FormData {
+  title: string
+  detail: string
+}
+
+const CreateTodoForm = () => {
+
+  const router = useRouter()
+  const { register, handleSubmit } = useForm<FormData>()
+
+  const onSubmit = async (data: FormData) => {
+    // 一緒にuserIDもTodoの中に登録させたい。
+    await addDoc(collection(db, 'todos'), { title: data.title, detail: data.detail, status: 'notStarted', uid: 'userID' })
+      .then((docRef) => {
+        console.log(docRef, "NewTodo has been added to Firebase")
+      })
+      .catch((e) => {
+        console.error('Error adding document: ', e)
+      })
+      .finally(() => console.log('finally'))
+    router.push('/')
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <VStack>
+          <label>新しいタスク</label>
+          <Input
+            type="text"
+            {...register("title", { required: true, min: 3, max: 20 })}
+          />
+          <label>タスクの詳細</label>
+          <Textarea
+            placeholder="Please write about this task"
+            {...register("detail")}
+          />
+          <Button bg={'lightBlue'} type="submit">Submit</Button>
+        </VStack>
+      </form>
+    </>
+  )
+}
+
+export default CreateTodoForm
